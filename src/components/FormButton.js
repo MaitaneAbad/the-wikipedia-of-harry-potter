483740648b8f78bb7133d525bcfc298d53463169
MaitaneAbad <maitaneabadconde@gmail.com>
2021-11-16 20:44:06 +0100

    add style button quiz

--- src/components/FormButton.js
+++ src/components/FormButton.js
@@ -1,12 +1,30 @@
 import { Link } from 'react-router-dom';
-
+import hat from '../images/hat.png';
+import '../styles/layout/formButton.scss';
 const FormButton = (props) => {
   return (
-    <Link to={'/quiz/'}>
-      <form>
-        <input type='button' value='Quiz' />
+    <>
+      <div className='quiz'>
+        {' '}
+        <p className='quiz__title'>¿Quieres conocer de qué casa eres?</p>
+        <p className='quiz__subtitle'> ¡Pues pincha en el sombrero!</p>
+      </div>
+      <form className='quiz__formButton'>
+        <Link to={'/quiz/'}>
+          <input
+            className='quiz__formButton--input'
+            type='button'
+            value='Quiz'
+          />
+          <img
+            className='quiz__formButton--img'
+            src={hat}
+            alt='cuestionario del sombrero seleccionador'
+            title='cuestionario del sombrero seleccionador'
+          />
+        </Link>
       </form>
-    </Link>
+    </>
   );
 };
 export default FormButton;
