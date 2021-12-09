bb544b298dac5b1fe6bed2bd0e35809a4dd61424
MaitaneAbad <maitaneabadconde@gmail.com>
2021-12-09 19:19:46 +0100

    change style parchment

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,29 +1,35 @@
 import { Link } from 'react-router-dom';
 const CharacterCard = (props) => {
   return (
-    <Link
-      to={`/character/${props.characterData.id}`}
-      className='sectionList__list--characterLi__link'
-    >
-      <img
-        className='sectionList__list--characterLi__link--img'
-        src={props.characterData.img}
-        alt={`Foto de: ${props.characterData.name}`}
-        title={`Foto de: ${props.characterData.name}`}
-      />
+    <div>
+      <Link
+        to={`/character/${props.characterData.id}`}
+        className='sectionList__list--characterLi__link'
+      >
+        <img
+          className='sectionList__list--characterLi__link--img'
+          src={props.characterData.img}
+          alt={`Foto de: ${props.characterData.name}`}
+          title={`Foto de: ${props.characterData.name}`}
+        />
 
-      <div className='sectionList__list--characterLi__link--containerData'>
-        <h2 className='sectionList__list--characterLi__link--containerData__name'>
-          {props.characterData.name}
-        </h2>
-        <h3 className='sectionList__list--characterLi__link--containerData__nickName'>
-          Nickname
-        </h3>
-        <p className='sectionList__list--characterLi__link--containerData__nickName--nick'>
-          {props.characterData.nickName}
-        </p>
-      </div>
-    </Link>
+        <div className='sectionList__list--characterLi__link--containerData'>
+          <h2 className='sectionList__list--characterLi__link--containerData__name'>
+            {props.characterData.name}
+          </h2>
+          <h3 className='sectionList__list--characterLi__link--containerData__nickName'>
+            Nickname
+          </h3>
+          <p className='sectionList__list--characterLi__link--containerData__nickName--nick'>
+            {props.characterData.nickName}
+          </p>
+        </div>
+      </Link>
+
+      <p className='sectionList__list--characterLi__link--paragraph'>
+        Pincha para más información
+      </p>
+    </div>
   );
 };
 export default CharacterCard;
