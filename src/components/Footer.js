58ffb200e913157116810f6a499eb524ce5f30e8
MaitaneAbad <maitaneabadconde@gmail.com>
2021-12-16 17:50:44 +0100

    change font size and style to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,34 @@
+import '../styles/layout/footer.scss';
 const Footer = () => {
   return (
     <>
       <footer className='footer'>
-        <p> &copy; Maitane Abad Conde - 2021 </p>
+        <p className='footer__title'> &copy; Maitane Abad Conde - 2021 </p>
+
+        <nav class='footer__nav'>
+          <ul class='footer__nav--list'>
+            <li class='footer__nav--list__link'>
+              <a
+                href='https://github.com/MaitaneAbad'
+                target='_blank'
+                rel='noreferrer'
+                title='Github de Maitane'
+              >
+                <i class='fab fa-github-square'></i>
+              </a>
+            </li>
+            <li class='footer__nav--list__link'>
+              <a
+                href='https://www.linkedin.com/in/maitaneabadconde/'
+                target='_blank'
+                rel='noreferrer'
+                title='Linkedin de Maitane'
+              >
+                <i class='fab fa-linkedin'></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </footer>
     </>
   );
