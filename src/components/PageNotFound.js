75b4364d4ba4a77ecc6edb0b3b9d2ebcc0254f3e
MaitaneAbad <maitaneabadconde@gmail.com>
2021-12-21 19:54:42 +0100

    add style page not found and animations

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,12 +1,24 @@
 import { Link } from 'react-router-dom';
 import '../styles/layout/pageNotFound.scss';
-
+import owl from '../images/owl.png';
 const PageNotFound = () => {
   return (
     <div className='pageNotFound'>
-      <p>Página no encontrada. Vuelve por el portal</p>
-
-      <Link to='/'></Link>
+      <p className='pageNotFound__paragraph'>
+        Oscuros y difíciles tiempos nos aguardan. Pronto todos tendremos que
+        elegir entre lo que es correcto y lo que es fácil
+      </p>
+      <Link to='/'>
+        <p className='pageNotFound__back'>
+          <img
+            className='pageNotFound__back--img'
+            src={owl}
+            title='Ayudate de Hedwig para volver a la página principal'
+            alt='Ayudate de Hedwig para volver a la página principal'
+          />
+          Ayudate de Hedwig para volver a la página principal.
+        </p>
+      </Link>
     </div>
   );
 };
